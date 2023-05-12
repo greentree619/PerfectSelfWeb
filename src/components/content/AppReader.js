2578af3981b6792b4120bf0932e651d06ececf7f
tonybingo-stack <tonybingo19970425@outlook.com>
2023-05-13 05:02:56 +1000

    css moved to scss

--- src/components/content/AppReader.js
+++ src/components/content/AppReader.js
@@ -1,38 +1,4 @@
 import React from 'react'
-// import {
-//     CCard,
-//     CCardBody,
-//     CCardGroup,
-//     CCardHeader,
-//     CCol,
-//     CLink,
-//     CRow,
-//     CWidgetStatsB,
-//     CWidgetStatsC,
-//     CWidgetStatsE,
-//     CWidgetStatsF,
-//   } from '@coreui/react'
-//   import { getStyle } from '@coreui/utils'
-//   import CIcon from '@coreui/icons-react'
-//   import {
-//     cilArrowRight,
-//     cilBasket,
-//     cilBell,
-//     cilChartPie,
-//     cilMoon,
-//     cilLaptop,
-//     cilPeople,
-//     cilSettings,
-//     cilSpeech,
-//     cilSpeedometer,
-//     cilUser,
-//     cilUserFollow,
-//   } from '@coreui/icons'
-// import { CChartBar, CChartLine } from '@coreui/react-chartjs'
-// import { CWidgetStatsD } from '@coreui/react'
-// import { cibFacebook, cibLinkedin, cibTwitter, cilCalendar } from '@coreui/icons'
-
-import './AppReader.css'
 
 const AppReader = () => {
   return (
