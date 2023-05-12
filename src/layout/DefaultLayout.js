f81fc9bbe3277a455e3bf40d0f7d24b369806e8c
greentree619 <greentree619@outlook.com>
2023-05-12 17:42:37 +0200

    Hom page layout ok.

--- src/layout/DefaultLayout.js
+++ src/layout/DefaultLayout.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { AppContent, AppSidebar, AppFooter, AppHeader } from '../components/index'
+import { AppContent, AppSidebar, AppFooter, AppHeader, AppBreadcrumb } from '../components/index'
 
 const DefaultLayout = () => {
   return (
@@ -7,7 +7,8 @@ const DefaultLayout = () => {
       <AppSidebar />
       <div className="wrapper d-flex flex-column min-vh-100 bg-light">
         <AppHeader />
-        <div className="body flex-grow-1 px-3">
+        <div className="body flex-grow-1">
+          <AppBreadcrumb />
           <AppContent />
         </div>
         <AppFooter />
